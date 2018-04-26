a6e31c5111d3365ed1b870649adab89c4770e771
Bartosz Karpiński <karpiq@gmail.com>
2018-04-26 16:09:10 +0200

    Reworked send_report date ranges, fixed localization for email report

--- apps/stats/management/commands/send_report.py
+++ apps/stats/management/commands/send_report.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from dateutil import parser as date_parser
 
 from django.core.management.base import BaseCommand
 from django.template.loader import get_template
@@ -14,23 +14,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('date_option')
+        parser.add_argument('date')
 
     def handle(self, *args, **options):
         date_option = options['date_option']
-        date_to = datetime.now().date()
+        input_date = date_parser.parse(options['date']).date()
         if date_option == 'week':
-            date_from = (date_to - relativedelta(weeks=1))
+            date_from = input_date.replace(day=(input_date.day - input_date.weekday()))
+            date_to = date_from + relativedelta(days=6)
             title = 'Raport tygodniowy'
         elif date_option == 'month':
-            date_from = (date_to - relativedelta(months=1))
+            date_from = input_date.replace(day=1)
+            date_to = date_from + relativedelta(months=1) - relativedelta(days=1)
             title = 'Raport miesięczny'
         elif date_option == 'year':
-            date_from = (date_to - relativedelta(years=1))
+            date_from = input_date.replace(day=1, month=1)
+            date_to = date_from + relativedelta(years=1) - relativedelta(days=1)
             title = 'Raport roczny'
         else:
             print("Invalid date option (allowed are: 'week', 'month', 'year').")
             return
 
+        print("Sending {} report from {} to {}".format(date_option, date_from, date_to))
         report_data = get_report_data(date_from, date_to)
         report_data['title'] = title
         report_data['date_from'] = date_from
