21ebdbec523ec7627b727fefe5913d13c4b80abc
Bartosz Karpiński <karpiq@gmail.com>
2019-07-30 15:32:58 +0200

    Add dropbox_load command for laoding backups

---
+++ KlimaKar/management/commands/dropbox_load.py
@@ -0,0 +1,64 @@
+import os
+import zipfile
+import dropbox
+import tempfile
+import shutil
+
+from dropbox.exceptions import AuthError
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from KlimaKar.settings import DROPBOX_TOKEN
+from apps.warehouse import models as warehouse
+from apps.invoicing import models as invoicing
+
+
+class Command(BaseCommand):
+    help = 'Loads backup from dropbox'
+    apps_to_load = ['warehouse', 'invoicing']
+
+    def add_arguments(self, parser):
+        parser.add_argument('--clear', action='store_true')
+
+    def handle(self, *args, **options):
+        print("Are you sure you want to load backup? (Y/n)")
+        if input() != 'Y':
+            return
+
+        if options['clear']:
+            self._clear_warehouse()
+            self._clear_invocing()
+
+        dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+        try:
+            dbx.users_get_current_account()
+        except AuthError:
+            print("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+            return
+        path = dbx.files_list_folder('').entries[-1].path_lower
+        metadata, response = dbx.files_download(path)
+        temp_dir = tempfile.mkdtemp()
+        path = os.path.join(temp_dir, 'backup.zip')
+        with open(path, 'wb') as backup:
+            backup.write(response.content)
+
+        with zipfile.ZipFile(path, 'r') as zip_ref:
+            zip_ref.extractall(temp_dir)
+
+        for app in self.apps_to_load:
+            call_command('loaddata', os.path.join(temp_dir, '{}.json'.format(app)))
+        shutil.rmtree(temp_dir)
+
+    def _clear_warehouse(self):
+        print('Deleted from warehouse:')
+        print(' ' * 4, warehouse.Invoice.objects.all().delete())
+        print(' ' * 4, warehouse.Ware.objects.all().delete())
+        print(' ' * 4, warehouse.Supplier.objects.all().delete())
+
+    def _clear_invocing(self):
+        print('Deleted from invoicing:')
+        print(' ' * 4, invoicing.CorrectiveSaleInvoice.objects.all().delete())
+        print(' ' * 4, invoicing.SaleInvoice.objects.all().delete())
+        print(' ' * 4, invoicing.Contractor.objects.all().delete())
+        print(' ' * 4, invoicing.ServiceTemplate.objects.all().delete())
