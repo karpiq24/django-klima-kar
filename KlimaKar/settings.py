780bbe70db1075955a335a8926e6c11c68c68af4
bartosz <bartosz@klimaserver.localdomain>
2019-10-20 20:01:30 +0200

    Fix staticfiles

--- KlimaKar/settings.py
+++ KlimaKar/settings.py
@@ -132,8 +132,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'KlimaKar/static/'),
 )
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
