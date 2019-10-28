a564d6de5d60a6742da3a5ead2e54b364bed246e
Bartosz Karpiński <karpiq@gmail.com>
2019-10-28 15:34:56 +0100

    Fix error when commission form is invalid, change order in commission form

--- apps/commission/functions.py
+++ apps/commission/functions.py
@@ -20,17 +20,21 @@ def check_uploaded_files(upload_key):
 def get_temporary_files(upload_key):
     directory = os.path.join(settings.TEMPORARY_UPLOAD_DIRECTORY, upload_key)
     files = []
-    for f in os.listdir(directory):
-        if f.endswith('meta'):
-            with open(os.path.join(directory, f)) as metafile:
-                files.append(json.loads(metafile.read()))
+    if not os.path.exists(directory):
+        return files
+    for f in [m for m in os.listdir(directory) if m.endswith('meta')]:
+        with open(os.path.join(directory, f)) as metafile:
+            files.append(json.loads(metafile.read()))
     return files
 
 
 def process_uploads(commission_pk, upload_key):
     files_added = 0
     directory = os.path.join(settings.TEMPORARY_UPLOAD_DIRECTORY, upload_key)
-    metafiles = [m for m in os.listdir(directory) if m.endswith('meta')]
+    metafiles = get_temporary_files(upload_key)
+    if not metafiles:
+        shutil.rmtree(directory)
+        return files_added
     try:
         commission = Commission.objects.get(pk=commission_pk)
     except Commission.DoesNotExist:
@@ -44,27 +48,23 @@ def process_uploads(commission_pk, upload_key):
             pk_dir = f['id']
             break
     for meta in metafiles:
-        meta_path = os.path.join(directory, meta)
-        with open(meta_path) as metafile:
-            data = json.loads(metafile.read())
-        file_path = os.path.join(directory, data['name'])
+        file_path = os.path.join(directory, meta['name'])
         if not os.path.exists(file_path):
-            os.remove(meta_path)
             continue
-        r = cloud.create_file(data['name'], open(file_path, 'rb').read(), pk_dir)
+        r = cloud.create_file(meta['name'], open(file_path, 'rb').read(), pk_dir)
         if r.status_code == 409:
             continue
         mch_id = None
         for f in cloud.get_files(pk_dir)['files']:
-            if f['name'] == data['name']:
+            if f['name'] == meta['name']:
                 mch_id = f['id']
                 break
         if mch_id:
             CommissionFile.objects.create(
                 commission=commission,
-                file_name=data['name'],
-                file_size=data['size'],
-                mime_type=data['type'],
+                file_name=meta['name'],
+                file_size=meta['size'],
+                mime_type=meta['type'],
                 mch_id=mch_id
             )
             files_added += 1
