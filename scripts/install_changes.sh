362bacb8b46275aa22fc3dfa4428b77d9772c72b
Bartosz Karpiński <karpiq@gmail.com>
2019-12-21 16:13:06 +0100

    Improve install script

--- scripts/install_changes.sh
+++ scripts/install_changes.sh
@@ -2,6 +2,12 @@ git fetch origin
 git rebase origin/master
 pip install -r docs/requirements.pip
 ./manage.py makemigrations
+./manage.py makemigrations KlimaKar
+./manage.py makemigrations commission
+./manage.py makemigrations invoicing
+./manage.py makemigrations settings
+./manage.py makemigrations stats
+./manage.py makemigrations warehouse
 ./manage.py migrate
 ./manage.py collectstatic --noinput
 sudo crontab < docs/crontab.txt
