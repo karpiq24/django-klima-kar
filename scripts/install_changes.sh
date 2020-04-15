87ad0a2d33b419a29a774cbbef9fa3a0e5e1a23a
Bartosz Karpiński <bartosz.karpinski@pwr.edu.pl>
2020-04-15 11:15:48 +0200

    Update install changes script

--- scripts/install_changes.sh
+++ scripts/install_changes.sh
@@ -3,10 +3,12 @@ git rebase origin/master
 pip install -r docs/requirements.pip
 ./manage.py makemigrations
 ./manage.py migrate
-cd tiles/
-npm ci
-npm run build
-cd ..
+if [ "$1" == "npm" ]; then
+    cd tiles/
+    npm ci
+    npm run build
+    cd ..
+fi
 ./manage.py collectstatic --noinput
 sudo crontab < docs/crontab.txt
 sudo service gunicorn restart
