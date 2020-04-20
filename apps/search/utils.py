b58c7d598014fce641a764f44f878711bff043be
Bartosz Karpiński <karpiq@gmail.com>
2020-04-20 16:55:56 +0200

    Improve indexing

--- apps/search/utils.py
+++ apps/search/utils.py
@@ -6,18 +6,6 @@ from django.apps import apps
 from apps.search.models import SearchDocument
 
 
-def reindex(content_type_pk, instance_pk):
-    model = ContentType.objects.get_for_id(content_type_pk).model_class()
-    instance = model.objects.get(pk=instance_pk)
-    SearchDocument.reindex(instance)
-
-
-def enqueue_reindex(instance):
-    model = instance._meta.model
-    content_type = ContentType.objects.get_for_model(model)
-    django_rq.enqueue(reindex, content_type.pk, instance.pk)
-
-
 def get_model(model_path):
     parts = model_path.split(".")
     model_name = parts[-1]
@@ -25,25 +13,47 @@ def get_model(model_path):
     return apps.get_model(app_name, model_name)
 
 
-def post_save_handler(sender, instance, **kwargs):
-    enqueue_reindex(instance)
-    for rel in getattr(sender, "RELATED_MODELS", []):
+def get_model_and_instance(content_type_pk, instance_pk):
+    model = ContentType.objects.get_for_id(content_type_pk).model_class()
+    return model, model.objects.get(pk=instance_pk)
+
+
+def update_index(content_type_pk, instance_pk):
+    model, instance = get_model_and_instance(content_type_pk, instance_pk)
+    SearchDocument.reindex(instance)
+    for rel in getattr(model, "RELATED_MODELS", []):
         model = get_model(rel[0])
-        objects = model.objects.filter(**{f"{rel[1]}": instance.pk})
+        objects = model.objects.filter(**{f"{rel[1]}": instance_pk})
         for obj in objects:
-            enqueue_reindex(obj)
+            SearchDocument.reindex(obj)
+
+
+def remove_from_index(content_type_pk, instance_pk):
+    model, instance = get_model_and_instance(content_type_pk, instance_pk)
+    SearchDocument.remove(instance)
+    for rel in getattr(model, "RELATED_MODELS", []):
+        model = get_model(rel[0])
+        objects = model.objects.filter(**{f"{rel[1]}": instance_pk})
+        for obj in objects:
+            SearchDocument.reindex(obj)
+
+
+def post_save_handler(sender, instance, **kwargs):
+    model = instance._meta.model
+    content_type = ContentType.objects.get_for_model(model)
+    django_rq.enqueue(update_index, content_type.pk, instance.pk)
 
 
 def pre_delete_handler(sender, instance, **kwargs):
-    SearchDocument.remove(instance)
-    for rel in getattr(sender, "RELATED_MODELS", []):
-        model = get_model(rel[0])
-        objects = model.objects.filter(**{f"{rel[1]}": instance.pk})
-        for obj in objects:
-            enqueue_reindex(obj)
+    model = instance._meta.model
+    content_type = ContentType.objects.get_for_model(model)
+    django_rq.enqueue(remove_from_index, content_type.pk, instance.pk)
 
 
 def update_parent_handler(sender, instance, **kwargs):
     parent = getattr(sender, "PARENT_FIELD", None)
     if parent:
-        enqueue_reindex(getattr(instance, parent))
+        parent_obj = getattr(instance, parent)
+        model = parent_obj._meta.model
+        content_type = ContentType.objects.get_for_model(model)
+        django_rq.enqueue(update_index, content_type.pk, parent_obj.pk)
