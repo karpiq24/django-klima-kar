a52f95c897470616bdd1db60aaaa24c2727c535d
Bartosz Karpiński <bartosz.karpinski@pwr.edu.pl>
2020-07-28 11:19:12 +0200

    Optimize webpack bundle file size

--- tiles/webpack.config.js
+++ tiles/webpack.config.js
@@ -1,17 +1,43 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+
 module.exports = {
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+                extractComments: "all",
+                uglifyOptions: {
+                    compress: true,
+                    output: null,
+                },
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ],
+    },
+    plugins: [
+        new CompressionPlugin({
+            test: /\.(js|css)/
+        }),
+        new UglifyJsPlugin(),
+    ],
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
-                }
+                    loader: "babel-loader",
+                },
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    }
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
