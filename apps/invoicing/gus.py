cecc080da12f0476d1f33935de2827b6a0020f87
Bartosz Karpiński <bartosz.karpinski@pwr.edu.pl>
2020-08-24 09:42:08 +0200

    Override GUSREGON to reduce timeout

--- apps/invoicing/gus.py
+++ apps/invoicing/gus.py
@@ -1,5 +1,25 @@
 from gusregon import GUS as GUSREGON
+from gusregon.gus import ENDPOINT_SANDBOX, WSDL
+from requests import Session
+from zeep import Client, Transport
+
 from KlimaKar import settings
+
+
+class GUSREGONTimeout(GUSREGON):
+    def __init__(self, api_key=None, sandbox=False):
+        if not any([api_key, sandbox]):
+            raise AttributeError("Api key is required.")
+        self.api_key = api_key
+        self.sandbox = sandbox
+        if sandbox:
+            self.api_key = api_key or "abcde12345abcde12345"
+            self.endpoint = ENDPOINT_SANDBOX
+        transport = Transport(session=Session(), timeout=5)
+        transport.session.headers = self.headers
+        client = Client(WSDL, transport=transport)
+        self.service = client.create_service("{http://tempuri.org/}e3", self.endpoint)
+        self.headers.update({"sid": self._service("Zaloguj", self.api_key)})
 
 
 class GUSSession(object):
@@ -13,9 +33,9 @@ class GUSSession(object):
     def reload_session(self):
         try:
             if settings.GUS_SANDBOX or not settings.GUS_KEY:
-                self.session = GUSREGON(sandbox=True)
+                self.session = GUSREGONTimeout(sandbox=True)
             else:
-                self.session = GUSREGON(api_key=settings.GUS_KEY)
+                self.session = GUSREGONTimeout(api_key=settings.GUS_KEY)
             return True
         except Exception:
             return False
